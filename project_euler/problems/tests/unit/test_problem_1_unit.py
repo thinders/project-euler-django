42ef73987433749cb25740e6a25983156bca4f71
Tyler Hinders <thinders@pillartechnology.com>
2018-08-08 14:38:55 -0500

    Add tests for problem 1

---
+++ project_euler/problems/tests/unit/test_problem_1_unit.py
@@ -0,0 +1,11 @@
+from django.test import SimpleTestCase
+
+from problems.solutions import problem_1
+
+
+class Problem1Tests(SimpleTestCase):
+    def test_solve(self):
+        self.assertEqual(23, problem_1.solve(10))
+
+    def test_solve_with_1000(self):
+        self.assertEqual(233168, problem_1.solve(1000))
