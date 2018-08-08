3ccc8220a3ddd458cc7437973f2cf2022f04c551
Tyler Hinders <thinders@pillartechnology.com>
2018-08-08 14:26:27 -0500

    Add problem 1 context

--- project_euler/problems/solutions/problem_1.py
+++ project_euler/problems/solutions/problem_1.py
@@ -1,11 +1,14 @@
+# Multiples of 3 and 5
+# Problem 1
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#
+# Find the sum of all the multiples of 3 or 5 below 1000.
 
-
-def solve():
-    MAX_NUMBER = 1000
+def solve(num=1000):
     total = 0
-    for x in range(5, MAX_NUMBER, 5):
+    for x in range(5, num, 5):
         total += x
-    for x in range(3, MAX_NUMBER, 3):
+    for x in range(3, num, 3):
         if x % 5 != 0:
             total += x
 
